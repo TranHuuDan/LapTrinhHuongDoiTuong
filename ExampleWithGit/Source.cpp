8a99312174da2fdb273b1bd9e213c9b340f4f230
TRẦN HỮU DẦN <1720040@student.hcmus.edu.vn>
2020-04-28 15:52:52 +0700

    LTDHT 28/3

--- ExampleWithGit/Source.cpp
+++ ExampleWithGit/Source.cpp
@@ -1,9 +1,20 @@
+
 #include<iostream>
-
+#include<cstdlib>
+#include<ctime>
 using namespace std;
-
-int main()
-{
-	cout << "hello world" << endl;
-	return 0;
+int main() {
+	int n; int s = 0;
+	cout << "Nhap n=";
+	cin >> n;
+	srand(time(0));
+	for (int i = 0; i < n; i++)
+	{
+		int x = rand() % 6 + 1;
+		cout << x << "\t";
+		s += x;
+	}
+	cout << endl;
+	cout << "s=" << s << endl;
+	 return 0;
 }
